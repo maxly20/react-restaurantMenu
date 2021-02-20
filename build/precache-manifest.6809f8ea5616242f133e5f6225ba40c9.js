90d3c68c1d59f83f51bed84ceac6dfae9ffcd926
maxly20 <maxly2011@gmail.com>
2021-02-20 12:16:02 +1100

    First Commit

---
+++ build/precache-manifest.6809f8ea5616242f133e5f6225ba40c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40175c7b0e82312c76d82d090e8b13b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5b8655080a99af405a3",
+    "url": "/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b8655080a99af405a3",
+    "url": "/static/js/main.8806300f.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
